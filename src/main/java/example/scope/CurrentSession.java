51f554824542a8ae1dffd68572bf5e418411c8b0
mpnsk <mpnsk@noreply.github.com>
2019-05-02 21:15:22 +0200

    Blank copy

---
+++ src/main/java/example/scope/CurrentSession.java
@@ -0,0 +1,23 @@
+package example.scope;
+
+import io.micronaut.context.annotation.AliasFor;
+import io.micronaut.context.annotation.Bean;
+import io.micronaut.runtime.context.scope.ScopedProxy;
+
+import javax.inject.Named;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@ScopedProxy
+@Documented
+@Retention(RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Bean
+public @interface CurrentSession {
+    @AliasFor(annotation = Named.class, member = "value")
+    String value() default "";
+}
